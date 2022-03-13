8aa8796bb1a57b17d577acdf421346f414280c54
alielrayes <61753691+alielrayes@users.noreply.github.com>
2022-03-13 19:35:45 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,133 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:flutter/material.dart';
+import 'package:quotes_app/widgets/card.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: QuoteApp(),
+    );
+  }
+}
+
+class QuoteApp extends StatefulWidget {
+  const QuoteApp({Key? key}) : super(key: key);
+
+  @override
+  State<QuoteApp> createState() => _QuoteAppState();
+}
+
+class BestQuotes {
+  String title;
+  String author;
+
+  BestQuotes({
+    required this.author,
+    required this.title,
+  });
+}
+
+class _QuoteAppState extends State<QuoteApp> {
+  List allQuotes = [
+    BestQuotes(author: "Ali Hassan", title: "رايق من نوعة فاخر 🔥"),
+    BestQuotes(
+        author: "Ali 7assan", title: "العقل السليم في البعد عن الحريم 😂"),
+    BestQuotes(
+        author: "Ali Elrayek",
+        title: "كُتر التفكير فى الى ضااااع هيعمل لك فى دماغك صادااااع  😉 "),
+    BestQuotes(
+        author: "ELRAYEK",
+        title: "فرح نفسك بنفسك ومتستناش حاجة حلوة من حد  ✋ "),
+  ];
+
+  delete(BestQuotes item) {
+    setState(() {
+      allQuotes.remove(item);
+    });
+  }
+
+  final myController1 = TextEditingController();
+  final myController2 = TextEditingController();
+
+  addNewQuote() {
+    setState(() {
+      allQuotes.add(
+        BestQuotes(author: myController2.text, title: myController1.text),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showModalBottomSheet(
+              context: context,
+              builder: (BuildContext context) {
+                return Container(
+                  padding: EdgeInsets.all(27),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      TextField(
+                          controller: myController1,
+                          maxLength: 20,
+                          decoration:
+                              InputDecoration(hintText: "Add new quote")),
+                      SizedBox(
+                        height: 22,
+                      ),
+                      TextField(
+                          controller: myController2,
+                          maxLength: 20,
+                          decoration: InputDecoration(hintText: "Add author")),
+                      SizedBox(
+                        height: 22,
+                      ),
+                      TextButton(
+                          onPressed: () {
+                            addNewQuote();
+                            Navigator.pop(context);
+                          },
+                          child: Text(
+                            "ADD",
+                            style: TextStyle(fontSize: 22),
+                          ))
+                    ],
+                  ),
+                );
+              },
+              isScrollControlled: true);
+        },
+        backgroundColor: Colors.redAccent,
+        child: Icon(Icons.add),
+      ),
+      appBar: AppBar(
+        backgroundColor: Color.fromARGB(255, 50, 57, 121),
+        title: Text(
+          "Best Quotes",
+          style: TextStyle(fontSize: 27),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: allQuotes
+              .map((item) => CardWidget(
+                    delete: delete,
+                    item: item,
+                  ))
+              .toList(),
+        ),
+      ),
+    );
+  }
+}
